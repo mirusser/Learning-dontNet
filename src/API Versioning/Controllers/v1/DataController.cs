458233d4638a4c719e5554bcbbc0768e5b3bcdda
mirusser <mirusser@gmail.com>
2021-04-11 13:57:23 +0200

    API varsioning: basic implementation of URL based API versioning

---
+++ src/API Versioning/Controllers/v1/DataController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API_Versioning.Controllers.v1
+{
+    [ApiVersion("1.0")]
+    [ApiController]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    public class DataController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "data from api v1";
+        }
+    }
+}
