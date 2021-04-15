235a5d07bd2f002ab1282a0d82187f26ac6e32db
mirusser <mirusser@gmail.com>
2021-04-15 20:52:54 +0200

    Implemented uploading, downloading and deleting files from database Improved file/index view

--- src/FileDemo/Controllers/FileController.cs
+++ src/FileDemo/Controllers/FileController.cs
@@ -40,6 +40,7 @@ namespace FileDemo.Controllers
             return View(fileuploadViewModel);
         }
 
+        #region Files to system/disk
         [HttpPost]
         public async Task<IActionResult> UploadToFileSystem(List<IFormFile> files, string description)
         {
@@ -79,7 +80,7 @@ namespace FileDemo.Controllers
             return RedirectToAction("Index");
         }
 
-        public async Task<IActionResult> DownloadFileFromFileSystem(int id)
+        public async Task<FileResult> DownloadFileFromFileSystem(int id)
         {
             var file = await _context.FilesOnFileSystem.Where(x => x.Id == id).FirstOrDefaultAsync();
             if (file == null) return null;
@@ -109,5 +110,52 @@ namespace FileDemo.Controllers
             TempData["Message"] = $"Removed {file.Name}.{file.Extension} successfully from File System.";
             return RedirectToAction(nameof(Index));
         }
+        #endregion
+
+        #region Files to database
+        [HttpPost]
+        public async Task<IActionResult> UploadToDatabase(List<IFormFile> files, string description)
+        {
+            foreach (var file in files)
+            {
+                var fileName = Path.GetFileNameWithoutExtension(file.FileName);
+                var extension = Path.GetExtension(file.FileName);
+                var fileModel = new FileOnDatabaseModel
+                {
+                    CreatedOn = DateTime.UtcNow,
+                    FileType = file.ContentType,
+                    Extension = extension,
+                    Name = fileName,
+                    Description = description
+                };
+                using (var dataStream = new MemoryStream())
+                {
+                    await file.CopyToAsync(dataStream);
+                    fileModel.Data = dataStream.ToArray();
+                }
+
+                _context.FilesOnDatabase.Add(fileModel);
+                _context.SaveChanges();
+            }
+            TempData["Message"] = "File successfully uploaded to Database";
+            return RedirectToAction("Index");
+        }
+
+        public async Task<FileResult> DownloadFileFromDatabase(int id)
+        {
+            var file = await _context.FilesOnDatabase.Where(x => x.Id == id).FirstOrDefaultAsync();
+            if (file == null) return null;
+            return File(file.Data, file.FileType, file.Name + file.Extension);
+        }
+
+        public async Task<IActionResult> DeleteFileFromDatabase(int id)
+        {
+            var file = await _context.FilesOnDatabase.Where(x => x.Id == id).FirstOrDefaultAsync();
+            _context.FilesOnDatabase.Remove(file);
+            _context.SaveChanges();
+            TempData["Message"] = $"Removed {file.Name + file.Extension} successfully from Database.";
+            return RedirectToAction("Index");
+        }
+        #endregion
     }
 }
