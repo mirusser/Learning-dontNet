db03feee740a424fe4e1293c0e210aabd49723dd
mirusser <mirusser@gmail.com>
2021-04-19 18:58:44 +0200

    Implemented InMemoryCachingDemo

---
+++ src/InMemoryCachingDemo/Controllers/CacheController.cs
@@ -0,0 +1,45 @@
+﻿using InMemoryCachingDemo.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InMemoryCachingDemo.Controllers
+{
+    [Route("api/[controller]")]
+    public class CacheController : ControllerBase
+    {
+        private readonly IMemoryCache _memoryCache;
+
+        public CacheController(IMemoryCache memoryCache)
+        {
+            _memoryCache = memoryCache;
+        }
+
+        [HttpGet("{key}")]
+        public IActionResult GetCache(string key)
+        {
+            _memoryCache.TryGetValue(key, out string value);
+
+            return Ok(value);
+        }
+
+        [HttpPost]
+        public IActionResult SetCache(CacheRequest data)
+        {
+            var cacheExpiryOptions = new MemoryCacheEntryOptions
+            {
+                AbsoluteExpiration = DateTime.Now.AddMinutes(5),
+                Priority = CacheItemPriority.High,
+                SlidingExpiration = TimeSpan.FromMinutes(2),
+                Size = 1024
+            };
+
+            _memoryCache.Set(data.key, data.value, cacheExpiryOptions);
+
+            return Ok();
+        }
+    }
+}
---
+++ src/InMemoryCachingDemo/Models/CacheRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InMemoryCachingDemo.Models
+{
+    public class CacheRequest
+    {
+        public string key { get; set; }
+        public string value { get; set; }
+    }
+}
