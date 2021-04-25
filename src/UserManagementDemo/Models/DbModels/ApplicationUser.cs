e2f02311abc3b26427d2c1513d2fbb9e02291f4a
mirusser <mirusser@gmail.com>
2021-04-25 09:37:28 +0200

    Started Implementing UserManagementDemo project

---
+++ src/UserManagementDemo/Models/DbModels/ApplicationUser.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UserManagementDemo.Models.DbModels
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        [DefaultValue(10)]
+        public int UsernameChangeLimit { get; set; }
+
+        public byte[] ProfilePicture { get; set; }
+    }
+}
---
+++ src/UserManagementDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace UserManagementDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
