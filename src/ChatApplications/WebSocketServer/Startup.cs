52c7b0643888cc86c503098022f23420dd906c9a
mirusser <mirusser@gmail.com>
2021-04-27 20:50:48 +0200

    Implemented base receiving message middleware for websocket server

--- src/ChatApplications/WebSocketServer/Startup.cs
+++ src/ChatApplications/WebSocketServer/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System.Net.WebSockets;
+using System.Threading;
 
 namespace WebSocketServer
 {
@@ -33,6 +34,20 @@ namespace WebSocketServer
                 {
                     WebSocket webSocket = await context.WebSockets.AcceptWebSocketAsync();
                     Console.WriteLine("WebSocket connected");
+
+                    await ReceiveMessage(webSocket, async (result, buffer) =>
+                    {
+                        if (result.MessageType == WebSocketMessageType.Text)
+                        {
+                            Console.WriteLine("Messagge received");
+                            return;
+                        }
+                        else if (result.MessageType == WebSocketMessageType.Close)
+                        {
+                            Console.WriteLine("Received close message");
+                            return;
+                        }
+                    });
                 }
                 else
                 {
@@ -59,7 +74,21 @@ namespace WebSocketServer
                 {
                     Console.WriteLine($" --> {header.Key} : {header.Value}");
                 }
+            }
+        }
 
+        private async Task ReceiveMessage(
+            WebSocket socket,
+            Action<WebSocketReceiveResult, byte[]> handleMessage)
+        {
+            var buffer = new byte[1024 * 4];
+
+            while (socket.State == WebSocketState.Open)
+            {
+                var result =
+                    await socket.ReceiveAsync(buffer: new ArraySegment<byte>(buffer), cancellationToken: CancellationToken.None);
+
+                handleMessage(result, buffer);
             }
         }
     }
