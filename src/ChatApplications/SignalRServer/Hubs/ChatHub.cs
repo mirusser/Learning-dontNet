617f3ec82ba8b0ef061ac0d82b9b4d33af68dbf6
mirusser <mirusser@gmail.com>
2021-04-29 19:20:16 +0200

    Implemented SignalR server for chat

---
+++ src/ChatApplications/SignalRServer/Hubs/ChatHub.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+using Newtonsoft.Json;
+
+namespace SignalRServer.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public override Task OnConnectedAsync()
+        {
+            Console.WriteLine($"Connection Established, connection Id: {Context.ConnectionId}");
+
+            Clients.Client(Context.ConnectionId).SendAsync($"Received connection Id: {Context.ConnectionId}");
+
+            return base.OnConnectedAsync();
+        }
+
+        public async Task SendMessageAsync(string message)
+        {
+            Console.WriteLine($"Message Recieved on: {Context.ConnectionId}");
+
+            var routeOb = JsonConvert.DeserializeObject<dynamic>(message);
+            string toClient = routeOb.To;
+
+            if (!string.IsNullOrEmpty(toClient))
+            {
+                await Clients.Client(toClient).SendAsync("ReceiveMessage", message);
+            }
+            else
+            {
+                await Clients.All.SendAsync("ReceiveMessage", message);
+            }
+        }
+    }
+}
---
+++ src/ChatApplications/SignalRServer/Startup.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using SignalRServer.Hubs;
+
+namespace SignalRServer
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddCors();
+            services.AddSignalR();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseCors(builder => builder
+                .WithOrigins("null") //headers with origin equals to null (for example running client from file)
+                .AllowAnyHeader()
+                .AllowAnyMethod()
+                .AllowCredentials());
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapHub<ChatHub>("/chatHub");
+            });
+        }
+    }
+}
