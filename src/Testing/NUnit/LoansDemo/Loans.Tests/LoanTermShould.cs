adf9d09770248ce219cc7312c597092cdaedd23d
mirusser <mirusser@gmail.com>
2021-05-06 22:59:49 +0200

    Added some base tests for collections, exceptions

--- src/Testing/NUnit/LoansDemo/Loans.Tests/LoanTermShould.cs
+++ src/Testing/NUnit/LoansDemo/Loans.Tests/LoanTermShould.cs
@@ -16,7 +16,85 @@ namespace Loans.Tests
         {
             var sut = new LoanTerm(1);
 
-            Assert.That(sut.ToMonths(), Is.EqualTo(12));
+            Assert.That(sut.ToMonths(), Is.EqualTo(12), "Months should be 12 * number of years");
+        }
+
+        [Test]
+        public void StoreYears()
+        {
+            var sut = new LoanTerm(1);
+
+            Assert.That(sut.Years, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void RespectValueEquality()
+        {
+            var a = new LoanTerm(1);
+            var b = new LoanTerm(1);
+
+            Assert.That(a, Is.EqualTo(b));
+        }
+
+        [Test]
+        public void RespectValueInEquality()
+        {
+            var a = new LoanTerm(1);
+            var b = new LoanTerm(2);
+
+            Assert.That(a, Is.Not.EqualTo(b));
+        }
+
+        [Test]
+        public void ReferenceEqualityExample()
+        {
+            var a = new LoanTerm(1);
+            var b = a;
+            var c = new LoanTerm(1);
+
+            Assert.That(a, Is.SameAs(b));
+            Assert.That(a, Is.Not.SameAs(c));
+
+            var x = new List<string> { "a", "b" };
+            var y = x;
+            var z = new List<string> { "a", "b" };
+
+            Assert.That(y, Is.SameAs(x));
+            Assert.That(z, Is.Not.SameAs(x));
+        }
+
+        [Test]
+        public void Double()
+        {
+            double a = 1.0 / 3.0;
+            Assert.That(a, Is.EqualTo(0.33).Within(0.004));
+            Assert.That(a, Is.EqualTo(0.33).Within(10).Percent);
+        }
+
+        [Test]
+        public void NotAllowedZeroYears()
+        {
+            Assert.That(() => new LoanTerm(0), Throws.TypeOf<ArgumentOutOfRangeException>());
+
+            Assert.That(() => new LoanTerm(0), Throws.TypeOf<ArgumentOutOfRangeException>()
+                .With
+                .Property("Message")
+                .EqualTo("Please specify a value greater than 0. (Parameter 'years')"));
+
+            Assert.That(() => new LoanTerm(0), Throws.TypeOf<ArgumentOutOfRangeException>()
+                .With
+                .Message
+                .EqualTo("Please specify a value greater than 0. (Parameter 'years')"));
+
+            Assert.That(() => new LoanTerm(0), Throws.TypeOf<ArgumentOutOfRangeException>()
+                .With
+                .Property("ParamName")
+                .EqualTo("years"));
+
+            Assert.That(() => new LoanTerm(0), Throws.TypeOf<ArgumentOutOfRangeException>()
+                .With
+                .Matches<ArgumentOutOfRangeException>(
+                    ex => ex.ParamName == "years"));
         }
     }
 }
---
+++ src/Testing/NUnit/LoansDemo/Loans.Tests/ProductComparerShould.cs
@@ -0,0 +1,107 @@
+﻿using Loans.Domain.Applications;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loans.Tests
+{
+    [TestFixture]
+    public class ProductComparerShould
+    {
+        [Test]
+        public void ReturnCorrectNumberOfComparisons()
+        {
+            var products = new List<LoanProduct>
+            {
+                new LoanProduct(1, "a", 1),
+                new LoanProduct(2, "b", 2),
+                new LoanProduct(3, "c", 3)
+            };
+
+            var sut = new ProductComparer(new LoanAmount("US", 200_000m), products);
+
+            List<MonthlyRepaymentComparison> comparisions = 
+                sut.CompareMonthlyRepayments(new LoanTerm(30));
+
+            Assert.That(comparisions, Has.Exactly(3).Items);
+        }
+
+        [Test]
+        public void NotReturnDuplicateComparisons()
+        {
+            var products = new List<LoanProduct>
+            {
+                new LoanProduct(1, "a", 1),
+                new LoanProduct(2, "b", 2),
+                new LoanProduct(3, "c", 3)
+            };
+
+            var sut = new ProductComparer(new LoanAmount("US", 200_000m), products);
+
+            List<MonthlyRepaymentComparison> comparisions =
+                sut.CompareMonthlyRepayments(new LoanTerm(30));
+
+            Assert.That(comparisions, Is.Unique);
+        }
+
+        [Test]
+        public void ReturnComparisonForFirstProduct()
+        {
+            var products = new List<LoanProduct>
+            {
+                new LoanProduct(1, "a", 1),
+                new LoanProduct(2, "b", 2),
+                new LoanProduct(3, "c", 3)
+            };
+
+            var sut = new ProductComparer(new LoanAmount("US", 200_000m), products);
+
+            //Need to also to know the expected monthly repayment
+            List<MonthlyRepaymentComparison> comparisions =
+                sut.CompareMonthlyRepayments(new LoanTerm(30));
+
+            var expectedProduct = new MonthlyRepaymentComparison("a", 1, 643.28m);
+
+            Assert.That(comparisions, Does.Contain(expectedProduct));
+        }
+
+        [Test]
+        public void ReturnComparisonForFirstProductWithPartialKnownExpectedValues()
+        {
+            var products = new List<LoanProduct>
+            {
+                new LoanProduct(1, "a", 1),
+                new LoanProduct(2, "b", 2),
+                new LoanProduct(3, "c", 3)
+            };
+
+            var sut = new ProductComparer(new LoanAmount("US", 200_000m), products);
+
+            List<MonthlyRepaymentComparison> comparisions =
+                sut.CompareMonthlyRepayments(new LoanTerm(30));
+
+            //Don't care about the expected monthly repayment,
+            //only thtat the product is there
+
+            //Assert.That(comparisions, Has.Exactly(1)
+            //                                .Property("ProductName")
+            //                                .EqualTo("a")
+            //                                .And
+            //                                .Property("InterestRate")
+            //                                .EqualTo(1)
+            //                                .And
+            //                                .Property("MonthlyRepayment")
+            //                                .GreaterThan(0));
+
+            Assert.That(comparisions, Has.Exactly(1)
+                                        .Matches<MonthlyRepaymentComparison>(
+                                        item => 
+                                            item.ProductName == "a" && 
+                                            item.InterestRate == 1 && 
+                                            item.MonthlyRepayment > 0));
+        }
+    }
+}
