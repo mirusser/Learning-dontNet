cb960f8e1be695ed1e2136c568ccc6a3c1633a1d
mirusser <mirusser@gmail.com>
2021-05-08 17:09:08 +0200

    Getting started with 'Moq'

---
+++ src/Testing/NUnit/LoansDemo_2/Loans.Tests/LoanApplicationProcessorShould.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Loans.Domain.Applications;
+using NUnit.Framework;
+using Moq;
+
+namespace Loans.Tests
+{
+    public class LoanApplicationProcessorShould
+    {
+        [Test]
+        public void DeclineLowSalary()
+        {
+            LoanProduct product = new(99, "Loan", 5.25m);
+            LoanAmount amount = new("USD", 200_000);
+            LoanApplication application =
+                new(42,
+                    product, 
+                    amount,
+                    "Sarah",
+                    25,
+                    "133 Pluralsight Drive, Draper, Utah",
+                    64_999);
+
+            var mockIdentityVerifier = new Mock<IIdentityVerifier>();
+            var mockCreditScorer = new Mock<ICreditScorer>();
+
+            //system under test
+            var sut = new LoanApplicationProcessor(mockIdentityVerifier.Object, mockCreditScorer.Object);
+
+            sut.Process(application);
+
+            Assert.That(application.GetIsAccepted(), Is.False);
+        }
+
+        [Test]
+        public void Accept()
+        {
+            LoanProduct product = new(99, "Loan", 5.25m);
+            LoanAmount amount = new("USD", 200_000);
+            LoanApplication application =
+                new(42,
+                    product,
+                    amount,
+                    "Sarah",
+                    25,
+                    "133 Pluralsight Drive, Draper, Utah",
+                    65_000);
+
+            var mockIdentityVerifier = new Mock<IIdentityVerifier>();
+            var mockCreditScorer = new Mock<ICreditScorer>();
+
+            //system under test
+            var sut = new LoanApplicationProcessor(mockIdentityVerifier.Object, mockCreditScorer.Object);
+
+            sut.Process(application);
+
+            Assert.That(application.GetIsAccepted(), Is.True);
+        }
+    }
+}
