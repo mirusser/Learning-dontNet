7dbb1c5eeff7c12609547270a83be952e714b636
mirusser <mirusser@gmail.com>
2021-05-20 20:27:28 +0200

    Init asp.net core integration tests

---
+++ src/Testing/TennisBookings/test/TennisBookings.Merchandise.Api.IntegrationTests/HealthCheckTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Testing;
+using NUnit.Framework;
+
+namespace TennisBookings.Merchandise.Api.IntegrationTests
+{
+    public class HealthCheckTests 
+    {
+        private HttpClient _httpClient;
+
+        [OneTimeSetUp]
+        public void OneTimeSetup()
+        {
+            WebApplicationFactory<Startup> factory = new();
+
+            _httpClient = factory.CreateDefaultClient();
+        }
+
+        [Test]
+        public async Task HealthCheckReturnsOk()
+        {
+            var response = await _httpClient.GetAsync("/health");
+
+            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+            //response.EnsureSuccessStatusCode();
+        }
+    }
+}
