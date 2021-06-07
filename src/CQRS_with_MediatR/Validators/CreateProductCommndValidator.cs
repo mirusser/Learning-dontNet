322cf338a26fb8b7d0f93a291c76b298ba6010d0
mirusser <mirusser@gmail.com>
2021-06-07 21:29:24 +0200

    MediatR Pipeline Behaviour - validation

---
+++ src/CQRS_with_MediatR/Validators/CreateProductCommndValidator.cs
@@ -0,0 +1,18 @@
+﻿using CQRS_with_MediatR.Features.ProductFeatures.Commands;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CQRS_with_MediatR.Validators 
+{
+    public class CreateProductCommndValidator : AbstractValidator<CreateProductCommand>
+    {
+        public CreateProductCommndValidator()
+        {
+            RuleFor(c => c.Name).NotEmpty();
+            RuleFor(c => c.Barcode).NotEmpty();
+        }
+    }
+}
