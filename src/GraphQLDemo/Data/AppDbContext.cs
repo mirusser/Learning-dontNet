7d3d802abbb8fec7e506d9f9aa03cb277aeb3fa6
mirusser <mirusser@gmail.com>
2021-07-29 19:48:25 +0200

    Added model, connection string, migration

---
+++ src/GraphQLDemo/Data/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using GraphQLDemo.Models;
+
+namespace GraphQLDemo.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Platform> Platforms{ get; set; }
+    }
+}
---
+++ src/GraphQLDemo/Models/Platform.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GraphQLDemo.Models
+{
+    public class Platform
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; } = string.Empty;
+        public string? LicenseKey { get; set; }
+
+        //public Platform(string name)
+        //{
+        //    Name = name;
+        //}
+    }
+}
