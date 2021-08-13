2074184978453f1e35b763efa18fb0b032a4f5d1
mirusser <mirusser@gmail.com>
2021-08-13 20:06:52 +0200

    Implemented very basic  demo of grpc server and client

---
+++ src/GrpcDemo/GrpcClient/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+using Grpc.Net.Client;
+using GrpcServer;
+
+namespace GrpcClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var helloRequest = new HelloRequest { Name = "Jon" };
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new Greeter.GreeterClient(channel);
+            var reply = await client.SayHelloAsync(helloRequest);
+
+            Console.ReadLine();
+        }
+    }
+}
