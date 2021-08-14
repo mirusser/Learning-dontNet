b20ee4f5e9e7b85ca5abb907249cd458c561020c
mirusser <mirusser@gmail.com>
2021-08-14 16:25:59 +0200

    Self hosted console app with DI (dependency injection)

---
+++ src/SelfHostedConsoleAppWIthDIDemo/Program.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Extensions.Hosting;
+using SelfHostedConsoleAppWIthDIDemo;
+using SelfHostedConsoleAppWIthDIDemo.Extensions;
+
+static IHostBuilder CreateHostBuilder(string[] args) =>
+    Host.CreateDefaultBuilder(args)
+        .UseStartup<Startup>();
+
+var host = CreateHostBuilder(args).Build();
+await host.RunAsync();
---
+++ src/SelfHostedConsoleAppWIthDIDemo/Startup.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace SelfHostedConsoleAppWIthDIDemo
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Configure your services here
+        }
+    }
+}
