7c2b4dea1c0082947804c48e8de13330f2503a9a
mirusser <mirusser@gmail.com>
2021-08-19 19:30:57 +0200

    Tidying up code a bit

--- src/RabbitMQ/HelloWorld/Send/Program.cs
+++ src/RabbitMQ/HelloWorld/Send/Program.cs
@@ -3,9 +3,7 @@ using RabbitMQ.Client;
 using System.Text;
 
 var factory = new ConnectionFactory() { HostName = "localhost" };
-
 using var connection = factory.CreateConnection();
-
 using var channel = connection.CreateModel();
 
 channel.QueueDeclare(queue: "hello", durable: false, exclusive: false, autoDelete: false, arguments: null);
