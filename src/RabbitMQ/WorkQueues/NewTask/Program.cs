d2c394b31c955137d361aa536815c7597fc79f6c
mirusser <mirusser@gmail.com>
2021-08-19 19:31:12 +0200

    RabbitMQ: Work queues

---
+++ src/RabbitMQ/WorkQueues/NewTask/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory() { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare(queue: "hello", durable: true, exclusive: false, autoDelete: false, arguments: null);
+
+var message = GetMessage(args);
+var body = Encoding.UTF8.GetBytes(message);
+var properties = channel.CreateBasicProperties();
+properties.Persistent = true;
+
+channel.BasicPublish(exchange: "", routingKey: "taks_queue", basicProperties: properties, body: body);
+
+static string GetMessage(string[] args)
+{
+    return ((args.Length > 0) ? string.Join(" ", args) : "Hello world");
+}
---
+++ src/RabbitMQ/WorkQueues/Worker/Program.cs
@@ -0,0 +1,40 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+var factory = new ConnectionFactory() { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare(queue: "taks_queue", durable: true, exclusive: false, autoDelete: false, arguments: null);
+
+//settings that tells RabbitMQ to dispatch message to the next worker that is not still busy.
+channel.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
+
+Console.WriteLine("Waiting for messages...");
+
+var consumer = new EventingBasicConsumer(channel);
+consumer.Received += (model, ea) =>
+{
+    var body = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine($"Received: {message}");
+
+    FakeTaskToSimulateExecutionTime(message);
+
+    Console.WriteLine("Done");
+
+    //acknowledging that message was processed (consumed properly)
+    channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
+};
+
+channel.BasicConsume(queue: "task_queue", autoAck: false, consumer: consumer);
+
+Console.ReadKey();
+
+void FakeTaskToSimulateExecutionTime(string message)
+{
+    int dots = message.Split('.').Length - 1;
+    Thread.Sleep(dots * 1000);
+}
