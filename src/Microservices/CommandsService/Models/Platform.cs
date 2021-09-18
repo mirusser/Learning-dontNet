d86c771be8ae7a5dc8f6fef751e9a0197b762013
mirusser <mirusser@gmail.com>
2021-09-18 19:22:26 +0200

    CommandsServise: data layer: models

---
+++ src/Microservices/CommandsService/Models/Platform.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandsService.Models
+{
+    public class Platform
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public int ExternalId { get; set; }
+
+        [Required]
+        public string Name { get; set; } = null!;
+
+        public ICollection<Command> Commands { get; set; } = new List<Command>();
+    }
+}
