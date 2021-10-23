1ab50f04dc67d0cf9272e3c0bfba76d1c997a9a4
mirusser <mirusser@gmail.com>
2021-10-23 20:39:40 +0200

    Integration test - getting data from db

---
+++ src/Testing/IntegrationTesting/AnimalControllerTests.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+using IntegrationTesting.Components.Introduction;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Xunit;
+
+namespace IntegrationTesting
+{
+    public class AnimalControllerTests
+    {
+        [Fact]
+        public void Should_AnimalController_ListAnimalsFromDatabase()
+        {
+            DbContextOptionsBuilder<AppDbContext> optionsBuilder = new();
+            optionsBuilder.UseInMemoryDatabase(MethodBase.GetCurrentMethod().Name);
+
+            using AppDbContext ctx = new(optionsBuilder.Options);
+            ctx.Add(new Animal() { Id = 1, Name = "Foo", Type = "Bar" });
+            ctx.SaveChanges();
+            var result = new AnimalController(ctx).List();
+
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            var animals = Assert.IsType<List<Animal>>(okResult.Value);
+            var animal = Assert.Single(animals);
+
+            Assert.NotNull(animal);
+            Assert.Equal(1, animal.Id);
+            Assert.Equal("Foo", animal.Name);
+            Assert.Equal("Bar", animal.Type);
+        }
+
+        [Fact]
+        public void Should_AnimalController_GetsAnimalFromDatabase()
+        {
+            DbContextOptionsBuilder<AppDbContext> optionsBuilder = new();
+            optionsBuilder.UseInMemoryDatabase(MethodBase.GetCurrentMethod().Name);
+
+            using AppDbContext ctx = new(optionsBuilder.Options);
+            ctx.Add(new Animal() { Id = 1, Name = "Foo", Type = "Bar" });
+            ctx.SaveChanges();
+            var result = new AnimalController(ctx).Get(1);
+
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            var animal = Assert.IsType<Animal>(okResult.Value);
+
+            Assert.NotNull(animal);
+            Assert.Equal(1, animal.Id);
+            Assert.Equal("Foo", animal.Name);
+            Assert.Equal("Bar", animal.Type);
+        }
+    }
+}
