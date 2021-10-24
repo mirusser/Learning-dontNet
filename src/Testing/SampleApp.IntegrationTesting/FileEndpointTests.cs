363a2b7cf91389852897ba39384d73cd0a218e10
mirusser <mirusser@gmail.com>
2021-10-24 15:51:52 +0200

    Added test SaveFileToDisk using create FileTestingFixture (with setup and clean up in it)

---
+++ src/Testing/SampleApp.IntegrationTesting/FileEndpointTests.cs
@@ -0,0 +1,32 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace SampleApp.IntegrationTesting
+{
+    public class FileEndpointTests : IClassFixture<FileTestingFixture>
+    {
+        private readonly FileTestingFixture _fileTestingFixture;
+
+        public FileEndpointTests(FileTestingFixture webApplicationFactory)
+        {
+            _fileTestingFixture = webApplicationFactory;
+        }
+
+        [Fact]
+        public async Task Should_SaveFileToDisk()
+        {
+            var client = _fileTestingFixture.CreateClient();
+
+            MultipartFormDataContent form = new();
+            form.Add(new StreamContent(_fileTestingFixture.TestFile), "file", "base.png");
+            var response = await client.PostAsync("/api/files", form);
+
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+
+            var fileResponse = await client.GetAsync("/test_images/base.png");
+            Assert.Equal(HttpStatusCode.OK, fileResponse.StatusCode);
+        }
+    }
+}
