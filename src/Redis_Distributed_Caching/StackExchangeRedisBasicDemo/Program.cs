99e069436714af21e7dd184308e73e61e4fcc092
mirusser <mirusser@gmail.com>
2021-10-31 19:22:43 +0100

    StackExchange.Redis and IDistributedCache basic usage

---
+++ src/Redis_Distributed_Caching/StackExchangeRedisBasicDemo/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Caching.StackExchangeRedis;
+using Microsoft.Extensions.Options;
+
+namespace StackExchangeRedisBasicDemo
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var options = Options.Create(new RedisCacheOptions()
+            {
+                Configuration = "127.0.0.1"
+            });
+
+            IDistributedCache cache = new RedisCache(options);
+
+            cache.SetString("rat", "bob");
+            Console.WriteLine(cache.GetString("rat"));
+
+            Console.ReadKey();
+        }
+
+        public static void RawStackExchangeRedis()
+        {
+            //var redis = ConnectionMultiplexer.Connect("127.0.0.1");
+            //var db = redis.GetDatabase();
+
+            //Console.WriteLine(db.StringGet("dog"));
+            //Console.WriteLine(db.StringGet("cat"));
+        }
+    }
+}
