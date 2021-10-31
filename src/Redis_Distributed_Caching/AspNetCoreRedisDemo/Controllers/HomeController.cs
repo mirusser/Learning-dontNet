e99a0e5fd1da2f51e39d66bcc9c983dcca6e46fe
mirusser <mirusser@gmail.com>
2021-10-31 19:32:35 +0100

    .net 5 IDistributedCache very basic usage (with DI)

---
+++ src/Redis_Distributed_Caching/AspNetCoreRedisDemo/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+
+namespace AspNetCoreRedisDemo.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> Index([FromServices] IDistributedCache cache)
+        {
+            return Ok(await cache.GetStringAsync("rat"));
+        }
+    }
+}
