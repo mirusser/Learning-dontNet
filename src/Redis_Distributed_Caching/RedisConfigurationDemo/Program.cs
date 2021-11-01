122befc033c4638368aa0c233a62a27d514dc2ad
mirusser <mirusser@gmail.com>
2021-11-01 16:27:08 +0100

    Very basic redis configuration

---
+++ src/Redis_Distributed_Caching/RedisConfigurationDemo/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Caching.StackExchangeRedis;
+using Microsoft.Extensions.Options;
+
+namespace RedisConfigurationDemo
+{
+    internal class Program
+    {
+        private static IDistributedCache _cache = new RedisCache(
+                Options.Create(new RedisCacheOptions()
+                {
+                    Configuration = "127.0.0.1:6379"
+                }));
+
+        public static void Main(string[] args)
+        {
+            //AbsoluteExpiration();
+
+            //SlidingExpiration();
+
+            var foo = _cache.GetString("key");
+        }
+
+        public static void AbsoluteExpiration()
+        {
+            var options = new DistributedCacheEntryOptions()
+            {
+                AbsoluteExpirationRelativeToNow = TimeSpan.FromSeconds(20)
+            };
+
+            _cache.SetString("key", "string", options);
+        }
+
+        public static void SlidingExpiration()
+        {
+            var options = new DistributedCacheEntryOptions()
+            {
+                SlidingExpiration = TimeSpan.FromSeconds(30)
+            };
+
+            _cache.SetString("key", "string", options);
+        }
+    }
+}
