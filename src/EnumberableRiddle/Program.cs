1fb0f65ebbd4318a6c88652ed06cdaafa64d7500
mirusser <mirusser@gmail.com>
2022-09-06 20:55:41 +0200

    Enumerable Riddle

---
+++ src/EnumberableRiddle/Program.cs
@@ -0,0 +1,17 @@
+﻿
+IEnumerable<Task> tasks = Enumerable.Range(0, 2)
+    .Select(_ => Task.Run(() => Console.Write("*")));
+
+await Task.WhenAll(tasks);
+
+Console.Write($"{tasks.Count()} stars");
+
+IEnumerable<Task> tasks1 = new[]
+{
+    Task.Run(() => Console.Write("*")),
+    Task.Run(() => Console.Write("*")),
+};
+
+await Task.WhenAll(tasks1);
+
+Console.Write($"{tasks1.Count()} stars");
