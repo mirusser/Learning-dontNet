746cf10a663868cd04e2252d4b89c5500fff686f
mirusser <mirusser@gmail.com>
2022-11-20 16:04:49 +0100

    Design pattern: structural: bridge

---
+++ src/DesignPatterns/Structural/Bridge/Bridge/ExtendedAbstraction.cs
@@ -0,0 +1,16 @@
+﻿namespace Bridge;
+
+// You can extend the Abstraction without changing the Implementation
+// classes.
+internal class ExtendedAbstraction : Abstraction
+{
+    public ExtendedAbstraction(IImplementation implementation) : base(implementation)
+    {
+    }
+
+    public override string Operation()
+    {
+        return "ExtendedAbstraction: Extended operation with:\n" +
+            base._implementation.OperationImplementation();
+    }
+}
---
+++ src/DesignPatterns/Structural/Bridge/Bridge/Program.cs
@@ -0,0 +1,13 @@
+﻿using Bridge;
+
+Client client = new();
+
+// The client code should be able to work with any pre-configured
+// abstraction-implementation combination.
+Abstraction abstraction = new(new ConcreteImplementationA());
+client.ClientCode(abstraction);
+
+Console.WriteLine();
+
+abstraction = new ExtendedAbstraction(new ConcreteImplementationB());
+client.ClientCode(abstraction);
