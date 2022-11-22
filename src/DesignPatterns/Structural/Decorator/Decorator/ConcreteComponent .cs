c201ffad0e107adce324fba139afbb82a3218e8f
mirusser <mirusser@gmail.com>
2022-11-22 19:30:23 +0100

    Design pattern: structural: decorator

---
+++ src/DesignPatterns/Structural/Decorator/Decorator/ConcreteComponent .cs
@@ -0,0 +1,11 @@
+﻿namespace Decorator;
+
+// Concrete Components provide default implementations of the operations.
+// There might be several variations of these classes.
+internal class ConcreteComponent : Component
+{
+    public override string Operation()
+    {
+        return "ConcreteComponent";
+    }
+}
---
+++ src/DesignPatterns/Structural/Decorator/Decorator/Decorator.cs
@@ -0,0 +1,34 @@
+﻿namespace Decorator;
+
+// The base Decorator class follows the same interface as the other
+// components. The primary purpose of this class is to define the wrapping
+// interface for all concrete decorators. The default implementation of the
+// wrapping code might include a field for storing a wrapped component and
+// the means to initialize it.
+public abstract class Decorator : Component
+{
+    protected Component component;
+
+    public Decorator(Component component)
+    {
+        this.component = component;
+    }
+
+    public void SetComponent(Component component)
+    {
+        this.component = component;
+    }
+
+    // The Decorator delegates all work to the wrapped component.
+    public override string Operation()
+    {
+        if (this.component != null)
+        {
+            return this.component.Operation();
+        }
+        else
+        {
+            return string.Empty;
+        }
+    }
+}
