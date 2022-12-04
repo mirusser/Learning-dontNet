980ec01bf10b44e5ffac57ed26e8e7c2361243dc
mirusser <mirusser@gmail.com>
2022-12-04 13:30:42 +0100

    Design patterns: Behavioral: Observer (aka publisher/subscriber)

---
+++ src/DesignPatterns/Behavioral/Observer/Observer/IObserver.cs
@@ -0,0 +1,7 @@
+﻿namespace Observer;
+
+public interface IObserver
+{
+    // Receive update from subject
+    void Update(ISubject subject);
+}
---
+++ src/DesignPatterns/Behavioral/Observer/Observer/Program.cs
@@ -0,0 +1,16 @@
+﻿// The client code.
+using Observer;
+
+Subject subject = new();
+ConcreteObserverA observerA = new ();
+subject.Attach(observerA);
+
+ConcreteObserverB observerB = new();
+subject.Attach(observerB);
+
+subject.SomeBusinessLogic();
+subject.SomeBusinessLogic();
+
+subject.Detach(observerB);
+
+subject.SomeBusinessLogic();
