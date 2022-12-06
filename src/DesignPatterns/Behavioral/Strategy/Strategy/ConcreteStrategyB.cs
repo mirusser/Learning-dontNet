480844ff887e1f6e6ab9be64d562b5d18907a1f7
mirusser <mirusser@gmail.com>
2022-12-06 18:57:54 +0100

    Design patterns: Behavioral: Strategy

---
+++ src/DesignPatterns/Behavioral/Strategy/Strategy/ConcreteStrategyB.cs
@@ -0,0 +1,13 @@
+﻿namespace Strategy;
+
+public class ConcreteStrategyB : IStrategy
+{
+    public object DoAlgorithm(object data)
+    {
+        var list = data as List<string>;
+        list.Sort();
+        list.Reverse();
+
+        return list;
+    }
+}
