cc8fa27f1859b52afdf7fe01333098ccb29e2eac
mirusser <mirusser@gmail.com>
2022-12-07 21:27:31 +0100

    Design patterns: Behavioral: Template Method

---
+++ src/DesignPatterns/Behavioral/TemplateMethod/TemplateMethod/Program.cs
@@ -0,0 +1,10 @@
+﻿using TemplateMethod;
+
+Console.WriteLine("Same client code can work with different subclasses:");
+
+Client.ClientCode(new ConcreteClass1());
+
+Console.Write("\n");
+
+Console.WriteLine("Same client code can work with different subclasses:");
+Client.ClientCode(new ConcreteClass2());
