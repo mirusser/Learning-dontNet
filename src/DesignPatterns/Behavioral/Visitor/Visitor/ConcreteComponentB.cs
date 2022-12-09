f4ea541cd2de18bc769eb290a70886cc8cdfe4a4
mirusser <mirusser@gmail.com>
2022-12-09 18:20:05 +0100

    Design patterns: Behavioral: Visitor

---
+++ src/DesignPatterns/Behavioral/Visitor/Visitor/ConcreteComponentB.cs
@@ -0,0 +1,15 @@
+﻿namespace Visitor;
+
+public class ConcreteComponentB : IComponent
+{
+    // Same here: VisitConcreteComponentB => ConcreteComponentB
+    public void Accept(IVisitor visitor)
+    {
+        visitor.VisitConcreteComponentB(this);
+    }
+
+    public string SpecialMethodOfConcreteComponentB()
+    {
+        return "B";
+    }
+}
