a2078376663e16d028f6c398bf4cb9fbb433ff87
mirusser <mirusser@gmail.com>
2022-12-30 19:16:14 +0100

    Another elastic search demo

---
+++ src/ElasticSearchDemo/AnotherDemo/Elk/Controllers/ProductsController.cs
@@ -0,0 +1,41 @@
+﻿using Elk.Models;
+using Microsoft.AspNetCore.Mvc;
+using Nest;
+
+namespace Elk.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class ProductsController : ControllerBase
+{
+    private readonly IElasticClient elasticClient;
+    private readonly ILogger<ProductsController> logger;
+
+    public ProductsController(
+        IElasticClient elasticClient,
+        ILogger<ProductsController> logger)
+    {
+        this.elasticClient = elasticClient;
+        this.logger = logger;
+    }
+
+    [HttpGet(Name = "GetProducts")]
+    public async Task<IActionResult> Get(string keyword)
+    {
+        var results = await elasticClient.SearchAsync<Product>(
+            s => s.Query(
+                q => q.QueryString(
+                    d => d.Query($"*{keyword}*")))
+            .Size(1000));
+
+        return Ok(results.Documents.ToList());
+    }
+
+    [HttpPost(Name = "AddProducts")]
+    public async Task<IActionResult> Post(Product product)
+    {
+        var response = await elasticClient.IndexDocumentAsync(product);
+
+        return Ok(response.Result);
+    }
+}
---
+++ src/ElasticSearchDemo/AnotherDemo/Elk/Extensions/ElasticSearchExtensions.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Elk.Models;
+using Nest;
+
+namespace Elk.Extensions;
+
+public static class ElasticSearchExtensions
+{
+    public static void AddElasticSearch(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddSingleton<IElasticClient>(_ =>
+        {
+            var url = configuration["ElkConfiguration:Uri"];
+            var defaultIndex = configuration["ElkConfiguration:Index"];
+
+            //var config = sp.GetRequiredService<IConfiguration>();
+            var settings = new ConnectionSettings(new Uri(url))
+                .PrettyJson()
+                .DefaultIndex(defaultIndex)
+                .AddDefaultMappings(defaultIndex)
+                .EnableDebugMode()
+                .EnableApiVersioningHeader(); //https://github.com/elastic/elasticsearch-net/issues/6154
+
+            var client =  new ElasticClient(settings);
+
+            CreateIndex(client, defaultIndex);
+
+            return client;
+        });
+    }
+
+    private static ConnectionSettings AddDefaultMappings(
+        this ConnectionSettings settings, 
+        string defaultIndex)
+    {
+        settings
+            .DefaultMappingFor<Product>(p => p
+                .Ignore(x=> x.Price)
+                .Ignore(x=> x.Id)
+                .Ignore(x=> x.Quantity)
+                .IndexName(defaultIndex));
+
+        return settings;
+    }
+
+    private static void CreateIndex(IElasticClient client, string indexName)
+    {
+        client.Indices.Create(indexName, i => i.Map<Product>(x => x.AutoMap()));
+    }
+}
