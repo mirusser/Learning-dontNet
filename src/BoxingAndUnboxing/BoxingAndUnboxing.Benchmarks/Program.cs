2a6a058abb23e4c31c201ce09bb51e3ba999f4af
mirusser <mirusser@gmail.com>
2023-01-08 17:50:30 +0100

    Simple boxing and unboxing benchmarks

---
+++ src/BoxingAndUnboxing/BoxingAndUnboxing.Benchmarks/Program.cs
@@ -0,0 +1,50 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+BenchmarkRunner.Run<Benchy>();
+
+[MemoryDiagnoser]
+public class Benchy
+{
+    private static readonly BoxingService BoxingService = new();
+    private static readonly object Boxed = 420;
+
+    [Benchmark]
+    public object BoxValue()
+        => BoxingService.BoxValue(420);
+
+    [Benchmark]
+    public int UnboxValue()
+        => BoxingService.UnboxValue(Boxed);
+
+    [Benchmark]
+    public int SimpleReturn()
+        => BoxingService.SimpleReturnInt(420);
+
+    [Benchmark]
+    public object SimpleReturnObject()
+        => BoxingService.SimpleReturnObject(Boxed);
+}
+
+public class BoxingService
+{
+    public object BoxValue(int number)
+    {
+        return number;
+    }
+
+    public int UnboxValue(object value)
+    {
+        return (int)value;
+    }
+
+    public int SimpleReturnInt(int number)
+    {
+        return number;
+    }
+
+    public object SimpleReturnObject(object value)
+    {
+        return value;
+    }
+}
---
+++ src/BoxingAndUnboxing/BoxingAndUnboxing/Program.cs
@@ -0,0 +1,14 @@
+﻿
+using System.Collections;
+
+int someNumber = 420;
+
+//implicit conversion (boxing)
+object someNumberObject = someNumber;
+
+// unboxing
+int unboxed = (int)someNumberObject;
+
+var arrayOfInts = Enumerable.Range(69, 420).ToArray();
+
+var arrayList = new ArrayList(arrayOfInts);
