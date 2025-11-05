6a7ae175cdc6e809ce704124cb6c9779151f59e7
mirusser <mirusser@gmail.com>
2025-11-05 17:25:45 +0100

    Few ways of creating strings - with benchmark and simple tests

---
+++ src/StringCreationDemo/StringCreationDemo/Program.cs
@@ -0,0 +1,60 @@
+﻿using System.Text;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+BenchmarkRunner.Run<Benchy>();
+
+[MemoryDiagnoser()]
+public class Benchy
+{
+    private const string ClearValue = "Password123!";
+    
+    [Benchmark]
+    public string MaskNaive()
+    {
+        var firstChars = ClearValue[..3];
+        var length = ClearValue.Length - firstChars.Length;
+
+        for (var i = 0; i < length; i++)
+        {
+            firstChars += "*";
+        }
+
+        return firstChars;
+    }
+
+    [Benchmark]
+    public string MaskStringBuilder()
+    {
+        var firstChars = ClearValue[..3];
+        var length = ClearValue.Length - firstChars.Length;
+        var stringBuilder = new StringBuilder(firstChars);
+
+        for (var i = 0; i < length; i++)
+        {
+            stringBuilder.Append('*');
+        }
+
+        return stringBuilder.ToString();
+    }
+    
+    [Benchmark]
+    public string MaskNewString()
+    {
+        var firstChars = ClearValue[..3];
+        var length = ClearValue.Length - firstChars.Length;
+        var asterisks = new string('*', length);
+        
+        return firstChars + asterisks;
+    }
+
+    [Benchmark]
+    public string MaskStringCreate()
+    {
+        return string.Create(ClearValue.Length, ClearValue, (span, value) =>
+        {
+            value.AsSpan().CopyTo(span);
+            span[3..].Fill('*');
+        });
+    }
+}
---
+++ src/StringCreationDemo/Tests/BenchyTests.cs
@@ -0,0 +1,52 @@
+﻿namespace Tests;
+
+using NUnit.Framework;
+
+[TestFixture]
+public class BenchyTests
+{
+    private string expectedValue = null!;
+    private Benchy sut = null!;
+
+    [OneTimeSetUp]
+    public void OneTimeSetup()
+    {
+        // Runs once before all tests in this class
+        expectedValue = "Pas*********";
+    }
+
+    [SetUp]
+    public void Setup()
+    {
+        // Runs before each test
+        sut = new Benchy();
+    }
+
+    [Test]
+    public void MaskNaive_ReturnsMaskedValue()
+    {
+        var result = sut.MaskNaive();
+        Assert.That(result, Is.EqualTo(expectedValue));
+    }
+
+    [Test]
+    public void MaskStringBuilder_ReturnsMaskedValue()
+    {
+        var result = sut.MaskStringBuilder();
+        Assert.That(result, Is.EqualTo(expectedValue));
+    }
+
+    [Test]
+    public void MaskNewString_ReturnsMaskedValue()
+    {
+        var result = sut.MaskNewString();
+        Assert.That(result, Is.EqualTo(expectedValue));
+    }
+
+    [Test]
+    public void MaskStringCreate_ReturnsMaskedValue()
+    {
+        var result = sut.MaskStringCreate();
+        Assert.That(result, Is.EqualTo(expectedValue));
+    }
+}
